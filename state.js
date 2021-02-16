5f845449752958881d54ccd68fd9dc8dfdc1ce7b
Laura <lauraporto596@gmail.com>
2021-02-16 11:25:51 +0100

    database mock definition 2

---
+++ state.js
@@ -0,0 +1,24 @@
+class Store {
+
+    state = [];
+
+    constructor() {
+        console.log("Se ha creado una instancia de Store");
+
+    };
+
+    async set(object) {
+        this.state.push(object);
+        return this.state.length;
+    };
+
+    async get() {
+        return this.state.slice(-10);
+        //el método .slice selecciona los últimos diez elementos marcando el número como negativo 
+    };
+};
+
+
+//Exportación de la clase
+//export default Simulator;
+module.exports = Store;
